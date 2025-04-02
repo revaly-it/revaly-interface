02732365d24457f52ce0d814d04db88e3c486424
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-02 14:12:47 -0300

    feat: structuring reviews and reports page

---
+++ src/app/profile/reviews/page.tsx
@@ -0,0 +1,26 @@
+import Footer from "@/organisms/footer/Footer";
+import Header from "@/organisms/header/Header";
+import ProfileForm from "@/organisms/profileForm/ProfileForm";
+import ProfileMenu from "@/organisms/profileMenu/profileMenu";
+
+export default function ReviewsPage() {
+    return (
+        <>
+        <Header />
+
+        <main className="max-w-screen-xl mx-auto p-6 text-white min-h-[80vh]">
+            <h1 className="text-3xl font-bold mb-8">Profile</h1>
+
+            <div className="flex flex-col md:flex-row gap-6">
+            <ProfileMenu />
+
+            <section className="flex-1">
+                <ProfileForm />
+            </section>
+            </div>
+        </main>
+
+        <Footer />
+        </>
+    );
+}
