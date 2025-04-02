b9564be19eb998e099fe7fd4c1d657eb01a97da3
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-02 15:04:56 -0300

    refactor: adding links to footer

--- src/organisms/footer/Footer.tsx
+++ src/organisms/footer/Footer.tsx
@@ -11,10 +11,10 @@ export default function Footer() {
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Linkedln</a>
+                            <a href="https://www.linkedin.com/in/rafael-aparecido-silva-oliveira/" className="hover:underline me-4 md:me-6">Linkedln</a>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">GitHub</a>
+                            <a href="https://github.com/orgs/revaly-it/repositories" className="hover:underline me-4 md:me-6">GitHub</a>
                         </li>
                     </ul>
                 </div>
